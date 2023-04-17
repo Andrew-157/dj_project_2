fc0389eccde815895da3fe468851238afc56e135
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-17 18:06:51 +0300

    Created needed models

--- articlee/urls.py
+++ articlee/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.
          as_view(template_name='users/password/password_reset_complete.html'), name='password_reset_complete'),
     path('', include('core.urls')),
+    path('', include('users.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
