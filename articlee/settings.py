76b5246c55cfaa8d48ebcb23f1c3aef86ea36b95
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-17 19:19:12 +0300

    Edited settings to connect mysql database

--- articlee/settings.py
+++ articlee/settings.py
@@ -86,13 +86,16 @@ WSGI_APPLICATION = 'articlee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'articlee',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'mysqlpassword'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
