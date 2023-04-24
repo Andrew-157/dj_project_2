449d81e444753ff8c072e8ee0d36f6d5150ad2a8
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-24 12:56:15 +0300

    Customized admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from core.models import CustomUser, Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'pub_date', 'tag_list']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).\
+            select_related('author').\
+            prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
