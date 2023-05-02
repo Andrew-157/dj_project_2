bd1efadfba5308eecf605353cb80be6eb6e9aba0
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-02 11:11:18 +0300

    Created model SocialMedia

--- core/models.py
+++ core/models.py
@@ -15,3 +15,21 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class SocialMedia(models.Model):
+    FACEBOOK = 'FB'
+    INSTAGRAM = 'IM'
+    YOUTUBE = 'YB'
+    TIKTOK = 'TT'
+    TWITTER = 'TW'
+    SOCIAL_MEDIA_TITLES = [
+        (FACEBOOK, 'Facebook'),
+        (INSTAGRAM, 'Instagram'),
+        (YOUTUBE, 'Youtube'),
+        (TIKTOK, 'TikTok'),
+        (TWITTER, 'Twitter')
+    ]
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    link = models.URLField(max_length=128, unique=True)
+    title = models.CharField(max_length=2, choices=SOCIAL_MEDIA_TITLES)
