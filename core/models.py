ae47ffec46773a2c0014518b76afcf7f55540504
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-08 11:32:16 +0300

    Renamed UserReadings to UserReading

--- core/models.py
+++ core/models.py
@@ -62,7 +62,7 @@ class Comment(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
-class UserReadings(models.Model):
+class UserReading(models.Model):
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     article = models.ForeignKey('core.Article', on_delete=models.CASCADE)
     date_read = models.DateTimeField()
